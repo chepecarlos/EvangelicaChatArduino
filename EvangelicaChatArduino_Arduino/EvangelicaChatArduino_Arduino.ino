1dd6354d7c376c3475c96ca6f92c9c09dd56934a
chepecarlos <chepecarlos@alswblog.org>
2018-04-03 19:51:01 -0600

    Codigo de comunicasion serial termninado

--- EvangelicaChatArduino_Arduino/EvangelicaChatArduino_Arduino.ino
+++ EvangelicaChatArduino_Arduino/EvangelicaChatArduino_Arduino.ino
@@ -1,9 +1,24 @@
-void setup() {
-  // put your setup code here, to run once:
+int boton = 2;
+int led = 9;
 
+void setup() {
+  pinMode(boton, INPUT_PULLUP);
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (digitalRead(boton) == 0 ) {
+    Serial.println("H");
+    delay(500);
+  }
+  if (Serial.available() ) {
+    char letra = Serial.read();
+    if (letra == 'H') {
+      digitalWrite(led, HIGH);
+    }
+    else if (letra == 'L') {
+      digitalWrite(led, LOW);
+    }
+  }
 }
